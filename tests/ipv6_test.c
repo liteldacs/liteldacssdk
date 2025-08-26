320ec9e30c6d0770a57c1da4a12567b7adc9159a
jiaxv <zoujiaxv7891@gmail.com>
2025-08-26 19:56:31 +0800

    25_8.26

--- tests/ipv6_test.c
+++ tests/ipv6_test.c
@@ -7,7 +7,7 @@
 #define V6_ADDR_LEN 16
 
 #pragma pack(1)
-typedef struct tcp_v6_s {
+typedef struct ipv6_tcp_s {
     uint8_t version;
     uint8_t traffic_class;
     uint32_t flow_label;
@@ -16,11 +16,21 @@ typedef struct tcp_v6_s {
     uint8_t hop_limit;
     buffer_t *src_address;
     buffer_t *dst_address;
+    uint16_t src_port;
+    uint16_t dst_port;
+    uint32_t sqn;
+    uint32_t ack;
+    uint8_t bias;
+    uint8_t reserve;
+    uint16_t flag;
+    uint16_t window;
+    uint16_t checksum;
+    uint16_t urgent;
     buffer_t *data;
-} tcp_v6_t;
+} ipv6_tcp_t;
 #pragma pack()
 
-static field_desc tcp_v6_fields[] = {
+static field_desc ipv6_tcp_fields[] = {
     {ft_set, 4, "VERSION", NULL},
     {ft_set, 8, "TRAFFIC CLASS", NULL},
     {ft_set, 20, "FLOW LABEL", NULL},
@@ -28,17 +38,28 @@ static field_desc tcp_v6_fields[] = {
     {ft_set, 8, "NEXT HEADER", NULL},
     {ft_set, 8, "HOP LIMIT", NULL},
     {ft_pad, 0, "PAD", NULL},
-    {ft_fl_str, 0, "IP SRC", &(pk_fix_length_t){.len = V6_ADDR_LEN}},
-    {ft_fl_str, 0, "IP DST", &(pk_fix_length_t){.len = V6_ADDR_LEN}},
+    {ft_fl_str, 0, "IP SRC", &(pk_fix_length_t){.len = 16}},
+    {ft_fl_str, 0, "IP DST", &(pk_fix_length_t){.len = 16}},
+    {ft_set, 16, "SRC PORT", NULL},
+    {ft_set, 16, "DST PORT", NULL},
+    {ft_set, 32, "SQN", NULL},
+    {ft_set, 32, "ACK", NULL},
+    {ft_set, 4, "BIAS", NULL},
+    {ft_set, 3, "PRESERVE", NULL},
+    {ft_set, 9, "FLAG", NULL},
+    {ft_set, 16, "WINDOW", NULL},
+    {ft_set, 16, "CHECKSUM", NULL},
+    {ft_set, 16, "URGENT", NULL},
+    {ft_pad, 0, "PAD", NULL},
     {ft_dl_str, 0, "DATA", NULL},
     {ft_pad, 0, "PAD", NULL},
     {ft_end, 0, NULL, NULL},
 };
-struct_desc_t tcp_v6_desc = {"TCP V6", tcp_v6_fields};
+struct_desc_t ipv6_tcp_desc = {"TCP V6", ipv6_tcp_fields};
 
 
 int main() {
-    tcp_v6_t v6 = {
+    ipv6_tcp_t v6 = {
         .version = 0x6,
         .traffic_class = 0x3,
         .flow_label = 0x1,
@@ -47,6 +68,16 @@ int main() {
         .hop_limit = 0x10,
         .src_address = init_buffer_unptr(),
         .dst_address = init_buffer_unptr(),
+        .src_port = 9456,
+        .dst_port = 9789,
+        .sqn = 1000,
+        .ack = 2000,
+        .bias = 0,
+        .reserve = 0x1,
+        .flag = 0x2,
+        .window = 500,
+        .checksum = 0xABCD,
+        .urgent = 0,
         .data = init_buffer_unptr()
     };
 
@@ -57,7 +88,7 @@ int main() {
     CLONE_TO_CHUNK(*v6.dst_address, addr.__in6_u.__u6_addr8, 16);
 
     CLONE_TO_CHUNK(*v6.data, "hello world", 11);
-    buffer_t *buf = gen_pdu(&v6, &tcp_v6_desc, "TCP V6");
+    buffer_t *buf = gen_pdu(&v6, &ipv6_tcp_desc, "TCP V6");
 
     log_buf(LOG_INFO, "IPV6", buf->ptr, buf->len);
 }
