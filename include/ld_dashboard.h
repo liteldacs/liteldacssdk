95ca55340f2b450fd086f8951b3870f1e3bb3ecb
jiaxv <jxuzou@163.com>
2025-10-05 04:37:42 +0800

    10.5

---
+++ include/ld_dashboard.h
@@ -0,0 +1,96 @@
+//
+// Created by jiaxv on 2025/10/5.
+//
+
+#ifndef LITELDACS_SDK_LD_DASHBOARD_H
+#define LITELDACS_SDK_LD_DASHBOARD_H
+
+#include "ld_json.h"
+#include "ld_net.h"
+
+typedef struct dashboard_obj_s {
+    pthread_t conn_th;
+    net_ctx_t net_ctx;
+    basic_conn_t *conn;
+}dashboard_obj_t;
+
+#define BACKEND_IP "127.0.0.1"
+#define BACKEND_PORT 9876
+
+typedef enum {
+    AS_REGISTER,
+    AS_UPDATE_COORDINATE,
+    DASHBOARD_START_STOP_AS,
+    GS_REGISTER,
+    DASHBOARD_SWITCH_AS, // dashboard指导GS切换AS
+    AS_GS_RECEIVED_MSG,
+    GS_ACCESS_AS,
+    GS_AS_EXITED,
+    DASHBOARD_SEND_SINGLE_DATA,
+    DASHBOARD_SEND_MULTI_DATA,
+
+    STOP_AS = 0xFF,
+}DASHBOARD_FUNCTION;
+
+typedef struct dashboard_func_define_s{
+    DASHBOARD_FUNCTION func_e;
+    json_tmpl_desc_t *tmpl;
+}dashboard_func_define_t;
+
+#pragma pack(1)
+typedef struct dashboard_data_s {
+    uint8_t type;
+    char *data;
+} dashboard_data_t;
+
+typedef struct dashboard_update_coordinate_s {
+    uint32_t UA;
+    double longitude;
+    double latitude;
+    bool is_direct;
+}dashboard_update_coordinate_t;
+
+typedef struct dashboard_register_gs_s {
+    uint16_t TAG;
+    double longitude;
+    double latitude;
+}dashboard_register_gs_t;
+
+typedef struct dashboard_switch_as_s {
+    uint32_t UA;
+    uint16_t GST_SAC;
+}dashboard_switch_as_t;
+
+typedef struct dashboard_received_msg_s {
+    uint8_t orient;
+    uint8_t type;
+    uint32_t sender;
+    uint32_t receiver;
+    buffer_t *data;
+}dashboard_received_msg_t;
+
+typedef struct dashboard_exit_as_s {
+    uint32_t UA;
+}dashboard_as_exit_t;
+
+typedef struct dashboard_as_info_upd_s {
+    uint32_t UA;
+    uint16_t AS_SAC;
+    uint16_t GS_SAC;
+}dashboard_as_info_upd_t;
+#pragma pack()
+
+extern json_tmpl_desc_t dashboard_data_tmpl_desc;
+
+extern const dashboard_func_define_t dashboard_func_defines[];
+
+void *dashboard_conn_connect(net_ctx_t *ctx, char *remote_addr, int remote_port, int local_port);
+
+l_err dashboard_data_send(dashboard_obj_t *dashboad_obj, DASHBOARD_FUNCTION func_e, void *data);
+
+void dashboard_conn_close(basic_conn_t *bc);
+
+l_err init_dashboard(dashboard_obj_t *dashboad_obj, recv_handler recv_handler);
+
+
+#endif //LITELDACS_SDK_LD_DASHBOARD_H
---
+++ src/ld_dashboard.c
@@ -0,0 +1,180 @@
+//
+// Created by jiaxv on 2025/10/5.
+//
+
+#include "ld_dashboard.h"
+
+
+static json_tmpl_t dashboard_data_tmpl[] = {
+    {cJSON_Number, sizeof(uint8_t), "type", "function", NULL},
+    {cJSON_Raw, sizeof(char *), "data", "data", NULL},
+    {cJSON_Invalid, 0, NULL, NULL, NULL}
+};
+
+static json_tmpl_t dashboard_update_coordinate_tmpl[] = {
+    {cJSON_Number, sizeof(uint32_t), "UA", "UA", NULL},
+    {cJSON_Number, sizeof(double), "longitude", "longitude", &isfloat},
+    {cJSON_Number, sizeof(double), "latitude", "latitude", &isfloat},
+    {cJSON_Number, sizeof(uint8_t), "isDirect", "isDirect", NULL},
+    {cJSON_Invalid, 0, NULL, NULL, NULL}
+};
+
+static json_tmpl_t dashboard_register_gs_tmpl[] = {
+    {cJSON_Number, sizeof(uint16_t), "tag", "tag", NULL},
+    {cJSON_Number, sizeof(double), "longitude", "longitude", &isfloat},
+    {cJSON_Number, sizeof(double), "latitude", "latitude", &isfloat},
+    {cJSON_Invalid, 0, NULL, NULL, NULL}
+};
+
+static json_tmpl_t dashboard_switch_as_tmpl[] = {
+    {cJSON_Number, sizeof(uint32_t), "UA", "UA", NULL},
+    {cJSON_Number, sizeof(uint16_t), "gst", "gst", NULL},
+    {cJSON_Invalid, 0, NULL, NULL, NULL}
+};
+
+static json_tmpl_t dashboard_received_msg_tmpl[] = {
+    {cJSON_Number, sizeof(uint8_t), "orient", "orient", NULL},
+    {cJSON_Number, sizeof(uint8_t), "type", "type", NULL},
+    {cJSON_Number, sizeof(uint32_t), "sender", "sender", NULL},
+    {cJSON_Number, sizeof(uint32_t), "receiver", "receiver", NULL},
+    {cJSON_String, sizeof(buffer_t *), "message", "message", NULL},
+    {cJSON_Invalid, 0, NULL, NULL, NULL}
+};
+
+static json_tmpl_t dashboard_as_exit_tmpl[] = {
+    {cJSON_Number, sizeof(uint32_t), "UA", "UA", NULL},
+    {cJSON_Invalid, 0, NULL, NULL, NULL}
+};
+
+static json_tmpl_t dashboard_as_info_upd_tmpl[] = {
+    {cJSON_Number, sizeof(uint32_t), "UA", "UA", NULL},
+    {cJSON_Number, sizeof(uint16_t), "AS_SAC", "AS_SAC", NULL},
+    {cJSON_Number, sizeof(uint16_t), "GS_SAC", "GS_SAC", NULL},
+    {cJSON_Invalid, 0, NULL, NULL, NULL}
+};
+
+json_tmpl_desc_t dashboard_data_tmpl_desc = {
+    .desc = "DASHBOARD_DATA",
+    .tmpl = dashboard_data_tmpl,
+    .size = sizeof(dashboard_data_t)
+};
+
+json_tmpl_desc_t dashboard_update_coordinate_tmpl_desc = {
+    .desc = "DASHBOARD_UPDATE_COORDINATE",
+    .tmpl = dashboard_update_coordinate_tmpl,
+    .size = sizeof(dashboard_update_coordinate_t)
+};
+
+json_tmpl_desc_t dashboard_register_gs_tmpl_desc = {
+    .desc = "DASHBOARD_REGISTER_GS",
+    .tmpl = dashboard_register_gs_tmpl,
+    .size = sizeof(dashboard_register_gs_t)
+};
+
+json_tmpl_desc_t dashboard_switch_as_tmpl_desc = {
+    .desc = "DASHBOARD_SWITCH_AS",
+    .tmpl = dashboard_switch_as_tmpl,
+    .size = sizeof(dashboard_switch_as_t)
+};
+
+json_tmpl_desc_t dashboard_received_msg_tmpl_desc = {
+    .desc = "DASHBOARD_RECEIVED_MSG",
+    .tmpl = dashboard_received_msg_tmpl,
+    .size = sizeof(dashboard_received_msg_t)
+};
+
+json_tmpl_desc_t dashboard_as_exit_tmpl_desc = {
+    .desc = "DASHBOARD_EXIT_AS",
+    .tmpl = dashboard_as_exit_tmpl,
+    .size = sizeof(dashboard_as_exit_t)
+};
+
+static json_tmpl_desc_t dashboard_as_info_upd_tmpl_desc = {
+    .desc = "DASHBOARD_AS_INFO",
+    .tmpl = dashboard_as_info_upd_tmpl,
+    .size = sizeof(dashboard_as_info_upd_t)
+};
+
+const dashboard_func_define_t dashboard_func_defines[] = {
+    {AS_REGISTER, &dashboard_update_coordinate_tmpl_desc},
+    {AS_UPDATE_COORDINATE, &dashboard_update_coordinate_tmpl_desc},
+    {DASHBOARD_START_STOP_AS, NULL},
+    {GS_REGISTER, &dashboard_register_gs_tmpl_desc},
+    {DASHBOARD_SWITCH_AS, &dashboard_switch_as_tmpl_desc},
+    {AS_GS_RECEIVED_MSG, &dashboard_received_msg_tmpl_desc},
+    {GS_ACCESS_AS, &dashboard_as_info_upd_tmpl_desc},
+    {GS_AS_EXITED, &dashboard_as_exit_tmpl_desc}
+};
+
+void *dashboard_conn_connect(net_ctx_t *ctx, char *remote_addr, int remote_port, int local_port) {
+    basic_conn_t *bc = malloc(sizeof(basic_conn_t));
+
+    bc->remote_addr = strdup(remote_addr);
+    bc->remote_port = remote_port;
+    bc->local_port = local_port;
+
+    if (init_basic_conn_client(bc, ctx, LD_TCP_CLIENT) == FALSE) {
+        return NULL;
+    }
+
+    return bc;
+}
+
+
+l_err dashboard_data_send(dashboard_obj_t *dashboad_obj, DASHBOARD_FUNCTION func_e, void *data) {
+    char *data_str = NULL;
+    if (get_json_str(data, dashboard_func_defines[func_e].tmpl, &data_str) != LD_OK) {
+        log_warn("Cannot generate JSON string");
+        return LD_ERR_INTERNAL;
+    }
+
+    dashboard_data_t to_resp = {
+        .type = (uint8_t)func_e,
+        .data = data_str,
+    };
+
+    char *root_s;
+    get_json_str(&to_resp, &dashboard_data_tmpl_desc, &root_s);
+
+    buffer_t *to_send = init_buffer_unptr();
+    CLONE_TO_CHUNK(*to_send, root_s, strlen(root_s));
+
+    // log_fatal("%s %d", root_s, strlen(root_s));
+    if (!dashboad_obj->net_ctx.send_handler) {
+        log_error("No valid dashboard sending handler");
+        return LD_ERR_NULL;
+    }
+    if (dashboad_obj->net_ctx.send_handler(dashboad_obj->conn, to_send, NULL, NULL) != LD_OK) {
+        log_error("Send Dashboard data Failed!");
+        return LD_ERR_INTERNAL;
+    }
+
+    free_buffer(to_send);
+    free(data_str);
+    free(root_s);
+
+    return LD_OK;
+}
+
+void dashboard_conn_close(basic_conn_t *bc) {
+    if (!bc) return;
+    free(bc);
+    log_warn("Closing connection!");
+}
+
+
+l_err init_dashboard(dashboard_obj_t *dashboad_obj, recv_handler recv_handler) {
+    dashboad_obj->net_ctx = (net_ctx_t){
+        .conn_handler = dashboard_conn_connect,
+        .recv_handler = recv_handler,
+        .close_handler = dashboard_conn_close,
+        .send_handler = defalut_send_pkt,
+        .epoll_fd = core_epoll_create(0, -1),
+    };
+
+    dashboad_obj->conn = client_entity_setup(&dashboad_obj->net_ctx, BACKEND_IP, BACKEND_PORT, 0); //不需要绑定本地端口
+    pthread_create(&dashboad_obj->conn_th, NULL, net_setup, &dashboad_obj->net_ctx);
+
+    pthread_detach(dashboad_obj->conn_th);
+    return LD_OK;
+}
